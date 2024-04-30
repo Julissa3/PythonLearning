d1dfd178f1dcc03dcbd65ffa5d52ea8d52d5ca60
Carbajal <jcarbajal13@cps.k12.il.us>
2024-04-30 11:57:08 -0500

    more conditionals

---
+++ day3.py
@@ -0,0 +1,66 @@
+# booleans
+# print(10 > 9)  # is this true or false
+# booleans are used to evaluate expressions and conditions in python
+# they are used to check if an expression is true or false
+
+# broughtFood = True
+# print(broughtFood)
+
+# is_raining = True
+# if is_raining:
+#     print("Take an umbrella!")
+# else:
+#     print("No umbrella needed.")
+
+# temperature = 35
+# if temperature > 30:
+#     print("It's warm")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("It's nice")
+# else:
+#     print("It's cold")
+# print("Done")
+
+
+# age = 12
+# if age >= 18:
+#     message = "Eligible"
+# else:
+#     message = "Not eligible"
+
+# message = "Eligible" if age >= 18 else "Not eligible" 
+# print(message)
+
+
+# high_income = False
+# good_credit = True
+# student = False
+
+# if (high_income or good_credit) and not student:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+
+# high_income = False
+# good_credit = True
+# student = True
+
+# if high_income or good_credit or not student:
+#     print("Eligible")
+
+
+# # age should be between 18 and 65
+# age = 22
+# if age >= 18 and age < 65:
+# if 18 <= age < 65
+#     print("Eligible")
+
+
+# if 10 == "10":
+#     print("a")
+# elif "bag" > "apple" and "bag" > "cat":
+#     print("b")
+# else:
+#     print("c")
