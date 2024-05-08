12a741f54867553cadcf374a6585558da27ba180
Carbajal <jcarbajal13@cps.k12.il.us>
2024-05-08 12:04:44 -0500

    added more content

---
+++ loops2.py
@@ -0,0 +1,23 @@
+# listOfNumbers = range(1,101)
+# print(listOfNumbers)
+# # A for loop so that you can loop the list of numbers
+# for number in listOfNumbers:
+#     # print(number)
+#     if number % 2 == 0:
+#         print(str(number)+ " is even")
+#     else:
+#         print(str(number) + " is odd")
+
+# creat a list of numbers from 1 to 10000
+listOfNumbers = range(1,10001)
+# print out the sum of all the numbers in the list
+print(f'the sum of the list is {sum(listOfNumbers)}')
+
+# total=0
+# for number in listOfNumbers:
+#     if number % 2 ==0:
+#         sumOfList = total + number
+#         print(sumOfList)
+#     else:
+#         print("This number is not")
+# If the number is even, add it to the sum
